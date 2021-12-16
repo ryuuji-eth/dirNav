60759420fb7a71d69f763918b46b97b6aff87d51
ryuuji-eth <96163677+ryuuji-eth@users.noreply.github.com>
2021-12-15 19:47:16 -0800

    Version 0.1.0
    This first version displays the current directory, asks user for what file extensions they want to save, if files with that extension exist, save to dictionary.

--- dirNavigate.py
+++ dirNavigate.py
@@ -5,36 +5,66 @@ dirNavigate.py v0.1.0
 Version Notes
 -------------
 0.1.0 Generic python script that works with the os library to navigate or display
-    items in a directory.
+    items in a directory. Asks user for file extension they'd like to save and if
+    that extension exists, it'll save that result to a dictionary.
 """
-
 import os
 
-
-
 def main():
-    cwd = workingDirectory()
-    exit()
+    print("Current directory is: ", workingDirectory())
+    displayFilesInDir()
+    ext = askFileExtension()
+    files2save = scanFileExtensions(workingDirectory(), ext) #Returns as a list
+    print(type(files2save))
+    exit() #Disable by commenting out
 
+# Define current path
 def workingDirectory():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     cwd = os.getcwd()
-    print("Current directory is: ", cwd)
-    displayWorkingDir()
     return cwd
 
-def displayWorkingDir():
+# Print the files in the current directory
+def displayFilesInDir():
+    allFiles = []
     print("Files in this directory are:")
     for i in os.scandir():
         if i.is_file():
+            allFiles.append(i.name)
             print(i.name)
+
+# Ask user for extension to type to save
+def askFileExtension():
+    answer = input("What extensions do you want to save? ")
+    dot  = "."
+    if dot in answer:
+        print("Extensions to be saved: ", answer)
+    else:
+        answer = dot + answer
+        print("Extension to be saved: ", answer)
+    #print(type(answer))
+    return answer
+
+# After taking user input, scan the working directory for files with that extension.
+# If files exist, append to a list and return the result
+def scanFileExtensions(cwd, answer):
+    extensionFilenames = []
+    for root, dirs, files in os.walk(cwd):
+        for file in files:
+            if file.endswith(answer):
+                print(file)	#Print json filenames here if you need to debug
+                extensionFilenames.append(file)
+        if len(extensionFilenames) == 0:
+            print("There are no files with the %s extension." % answer)
+            break
+    return extensionFilenames
 
 #Keeps the script from autoclosing after running. Disable by not calling.
 def exit():
-	k = 0
-	while k != '1':
-		k = input("Press 1 to exit: ")
-		print(k)
+    k = 0
+    while k != '1':
+        k = input("Press 1 to exit: ")
+        print(k)
 
 if __name__ == "__main__":
-	main()
+    main()
